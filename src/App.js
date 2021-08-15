3f83bb4d404446dd3e58ce68767c2644398074a8
Malena Vero <58344888+malenavero@users.noreply.github.com>
2021-08-15 18:35:33 -0300

    mejora fondos

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import './App.css';
 import { useState } from "react";
 
 import img0 from "./img/patterns/0.jpg"
-import img1 from "./img/patterns/1.jpg"
+import img1 from "./img/patterns/1.png"
 import img2 from "./img/patterns/2.jpg"
 import img3 from "./img/patterns/3.jpg"
 import img4 from "./img/patterns/4.jpg"
-import img5 from "./img/patterns/5.jpg"
+import img5 from "./img/patterns/5.png"
 import img6 from "./img/patterns/6.jpg"
 
 function App() {
